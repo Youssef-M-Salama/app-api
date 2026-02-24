032021fca69c7315bf99ebc449cdae191383e68d
youssef-salama <youssifsalama47@gmail.com>
2026-02-24 02:02:23 +0200

    feat: configure Swagger and API versioning

---
+++ App.Api/Controllers/v1/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class TestController : CustomControllerBase
+    {
+        /// <summary>
+        /// Handles HTTP GET requests and returns a simple greeting message.
+        /// </summary>
+        /// <returns>An <see cref="IActionResult"/> containing the string "Hello World" with an HTTP 200 OK status.</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
