82fd9097d213227dd2d2f1faa49093de8b2bd60a
youssef-salama <youssifsalama47@gmail.com>
2026-02-27 08:04:25 +0200

    fix: remove unsuitable validation attribute from registerDTO

--- App.Core/DTO/Request/RegisterDTO.cs
+++ App.Core/DTO/Request/RegisterDTO.cs
@@ -1,35 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace App.Core.DTO.Request
 {
     public class RegisterDTO
     {
-        [Required(ErrorMessage = "Person Name can't be blank")]
-        public string PersonName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Account type is required")]
+        [RegularExpression("^(charity|donor_organization)$", ErrorMessage = "Account type must be 'charity' or 'donor_organization'")]
+        public string AccountType { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Name can't be blank")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 200 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Username can't be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+        [Remote(action: "IsUsernameAlreadyRegistered", controller: "Account", ErrorMessage = "Username is already in use")]
+        public string Username { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email can't be blank")]
-        [EmailAddress(ErrorMessage = "Email should be in a proper email address format")]
-        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is already is use")]
+        [EmailAddress(ErrorMessage = "Email should be in proper format")]
+        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is already registered")]
         public string Email { get; set; } = string.Empty;
 
-
         [Required(ErrorMessage = "Phone number can't be blank")]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number should contain digits only")]
-        [Remote(action: "IsEmailAlreadyRegister", controller: "Account", ErrorMessage = "Email is already is use")]
-        public string PhoneNumber { get; set; } = string.Empty;
-
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string Phone { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password can't be blank")]
+        [MinLength(5, ErrorMessage = "Password must be at least 5 characters")]
         public string Password { get; set; } = string.Empty;
 
-
-        [Required(ErrorMessage = "Confirm Password can't be blank")]
-        [Compare("Password", ErrorMessage = "Password and confirm password do not match")]
+        [Required(ErrorMessage = "Confirm password can't be blank")]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirm password do not match")]
         public string ConfirmPassword { get; set; } = string.Empty;
+
+        
     }
 }
---
+++ App.Core/ServiceContracts/IAccountService.cs
@@ -0,0 +1,11 @@
+using App.Core.DTO.Request;
+using App.Core.DTO.Response;
+using App.Core.DTO.ResultPattern;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace App.Core.ServiceContracts;
+
+public interface IAccountService
+{
+    // public async ServiceResult<AuthResponseDto> RegisterAsync(RequestBodyPipeFeature registerRequest);
+}
